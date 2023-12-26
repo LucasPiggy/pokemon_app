fe0ce2e5deaf7f17cb1bb4838854dfb18553b3d8
LucasPiggy <95934048+LucasPiggy@users.noreply.github.com>
2023-12-26 16:59:53 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,3 +5,5 @@ st.set_page_config(
     page_title="Onta pokemon?",
     page_icon="🔅"
 )
+
+st.markdown("<h1 style='text-align: center; color: grey;'>¿Qué Pokémon Buscas?</h1>", unsafe_allow_html=True)
