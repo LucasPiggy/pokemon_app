233d95684b87c215d503113789cab73c4a080d77
LucasPiggy <95934048+LucasPiggy@users.noreply.github.com>
2023-12-26 16:39:15 +0100

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import base64
+from st_clickable_images import clickable_images
+from st_pages import Page, show_pages, hide_pages
+from streamlit_extras.switch_page_button import switch_page
+from streamlit_modal import Modal
+
+st.set_page_config(
+    page_title="Onta pokemon?",
+    page_icon="🔅"
+)
