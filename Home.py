8bf8d99ea0fb030f790bdf0788882902da27a9b3
LucasPiggy <95934048+LucasPiggy@users.noreply.github.com>
2023-12-27 10:04:34 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,7 +8,9 @@ st.set_page_config(
 
 st.markdown("<h1 style='text-align: center; color: grey;'>¿Qué Pokémon Buscas?</h1>", unsafe_allow_html=True)
 
-st.selectbox('Selecciona un tipo', ["Water", "Grass", "Fire"], placeholder = "Ningún tipo seleccionado")
+st.selectbox('Selecciona un tipo', ["Water", "Grass", "Fire"], 
+             index = None,
+             placeholder = "Ningún tipo seleccionado")
 
 st.image('./mapa.png')
 
