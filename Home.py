2cdc015903b7534f351149c4ce4deeb4ca6d04b9
LucasPiggy <95934048+LucasPiggy@users.noreply.github.com>
2023-12-27 19:43:48 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -9,10 +9,10 @@ st.set_page_config(
 st.markdown("<h1 style='text-align: center; color: grey;'>¿Qué Pokémon Buscas?</h1>", unsafe_allow_html=True)
 
 # Columnas y tabs de posición
-col1, col2 = st.columns(2)
 tab1, tab2 = st.tabs(["Ruta", "Pokémon"])
 
 with tab1:
+    col1, col2 = st.columns(2)
     nombre_ruta = st.text_input('Buscador de ruta por nombre')
     st.text(nombre_ruta)
 
