0a31b6352ff48f8d20302f1b808bb416b138e3f8
LucasPiggy <95934048+LucasPiggy@users.noreply.github.com>
2023-12-27 19:43:02 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,13 +8,9 @@ st.set_page_config(
 
 st.markdown("<h1 style='text-align: center; color: grey;'>¿Qué Pokémon Buscas?</h1>", unsafe_allow_html=True)
 
-st.selectbox('Selecciona un tipo', ["Water", "Grass", "Fire"], 
-             index = None,
-             placeholder = "Ningún tipo seleccionado")
-
 # Columnas y tabs de posición
 col1, col2 = st.columns(2)
-tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
+tab1, tab2 = st.tabs(["Ruta", "Pokémon"])
 
 with tab1:
     nombre_ruta = st.text_input('Buscador de ruta por nombre')
