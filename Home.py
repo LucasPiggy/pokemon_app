aa1f59c4571825f247017d3c8478612b486b995a
LucasPiggy <95934048+LucasPiggy@users.noreply.github.com>
2024-04-16 23:00:33 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -69,7 +69,7 @@ with tab_ruta:
             contents4 = file_.read()
             data_url4 = base64.b64encode(contents4).decode("utf-8")
             file_.close()
-            st.markdown(f'<img src="data:image/gif;base64,{data_url4}" width="450" height="400" alt="gif">',
+            st.markdown(f'<img src="data:image/gif;base64,{data_url4}" width="800" height="400" alt="gif">',
                                                 unsafe_allow_html=True)
    
 
