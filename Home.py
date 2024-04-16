354a2a08accf43970e35af314edd7a910d395f7c
LucasPiggy <95934048+LucasPiggy@users.noreply.github.com>
2024-04-16 22:02:59 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -26,8 +26,34 @@ with tab_ruta:
     st.table(search_res)
     
     st.image('./mapa.png')
-
-    
+    with open("encounters.json", "r") as file:
+        encounters = json.load(file)
+        if nombre_ruta != "":
+            try:
+                resultados = encounters["lugares"][0][nombre_ruta][0]
+                for tipo in resultados:
+                    n = 0
+                    res = resultados[tipo]
+                    st.subheader(tipo.upper().replace("-"," "))
+                    col1, col2, col3, col4, col5 = st.columns(5)
+                    columnas = [col1, col2, col3, col4, col5]
+                    for pok in res:
+                        with columnas[n%5]:
+                            st.text(list(pok.keys())[0])
+                            st.text(list(pok.values())[0])
+                            funciones.get_sprite(list(pok.keys())[0])
+                            file_ = open("spriteGIF.gif", "rb")
+                            contents = file_.read()
+                            data_url = base64.b64encode(contents).decode("utf-8")
+                            file_.close()
+                            dim = Image.open("spriteGIF.gif").size
+                            w = dim[0] * 1.8
+                            h = dim[1] * 1.8
+                            st.markdown(f'<img src="data:image/gif;base64,{data_url}"  width={w} height={h} alt="cat gif">',
+                                            unsafe_allow_html=True)
+                            n += 1
+            except:
+                st.text("Nombre de ruta incorrecto")
    
 
 with tab_pok:
