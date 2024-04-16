75a8a3ad9227335ccf95d82cbb060ef0e73e051a
LucasPiggy <95934048+LucasPiggy@users.noreply.github.com>
2024-04-16 22:18:51 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,6 +4,11 @@ import json
 import funciones
 from PIL import Image
 import pandas as pd
+import numpy as np
+import cv2
+import base64
+import imageio as iio
+from PIL import Image
 
 st.set_page_config(
     page_title="Onta pokemon?",
@@ -26,34 +31,15 @@ with tab_ruta:
     st.table(search_res)
     
     st.image('./mapa.png')
-    with open("encounters.json", "r") as file:
-        encounters = json.load(file)
-        if location != "":
-            try:
-                resultados = encounters["lugares"][0][location][0]
-                for tipo in resultados:
-                    n = 0
-                    res = resultados[tipo]
-                    st.subheader(tipo.upper().replace("-"," "))
-                    col1, col2, col3, col4, col5 = st.columns(5)
-                    columnas = [col1, col2, col3, col4, col5]
-                    for pok in res:
-                        with columnas[n%5]:
-                            st.text(list(pok.keys())[0])
-                            st.text(list(pok.values())[0])
-                            funciones.get_sprite(list(pok.keys())[0])
-                            file_ = open("spriteGIF.gif", "rb")
-                            contents = file_.read()
-                            data_url = base64.b64encode(contents).decode("utf-8")
-                            file_.close()
-                            dim = Image.open("spriteGIF.gif").size
-                            w = dim[0] * 1.8
-                            h = dim[1] * 1.8
-                            st.markdown(f'<img src="data:image/gif;base64,{data_url}"  width={w} height={h} alt="cat gif">',
-                                            unsafe_allow_html=True)
-                            n += 1
-            except:
-                st.text("Nombre de ruta incorrecto")
+
+    map_file = open(f"./hoenn_json.json")
+    map_data = json.load(map_file)
+
+    imdata = base64.b64decode(map_data["imageData"])
+    npimg = np.frombuffer(imdata, dtype=np.uint8);
+    image = cv2.imdecode(npimg, 1)
+
+    st.image(image)
    
 
 with tab_pok:
