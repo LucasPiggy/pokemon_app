c4e1054da33149de0fb6261a50699c09209391df
LucasPiggy <95934048+LucasPiggy@users.noreply.github.com>
2024-04-18 18:57:09 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -94,7 +94,7 @@ with tab_ruta:
                                                             unsafe_allow_html=True)
                         prob = int(search.loc[search["pokemon"] == pok]["prob"])
                         for i in funciones.get_tipo(pok):
-                            st.image(Image.open(f'./tipos/{i}.png'))
+                            st.image(Image.open(f'./tipos/{i}.png'), width = 40)
                         st.text(f"{prob}%")
                     n += 1
             
